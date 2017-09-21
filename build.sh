be9c3f174ddd2bc66b32affcad7f7728bc4af6bf
Perry A. Stoll <perry@pstoll.com>
2017-09-21 18:29:27 -0400

    Added build script for versioned deployment to static site

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dt=$(date '+%Y%m%d_%H%M%S');
+dir="resume-$dt"
+tgz="$dir.tgz"
+mkdir $dir/ 
+cp -r _site $dir/$dir # yes, duplicate
+pushd $dir
+  for ll in $(cat ../links.txt); do echo "linking $ll to resume-$dir"; ln -s $dir $ll; done
+popd
+chmod -R 0755 $dir
+tar -zcvf $tgz $dir
+echo $dir > latest-built.txt
+
+
+
